0ac47585192600b9fc1458a3e1eb73217e7d3493
Mohd Javed <63085243+MohdJavedumar@users.noreply.github.com>
2023-11-22 10:15:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-streamlit.title(' ]My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text(' Kale, spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑 🍞 Avocado Toast')
 
