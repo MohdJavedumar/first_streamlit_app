235e41bae7779e2acd1dfa3706f202d7d5caed7f
Mohd Javed <63085243+MohdJavedumar@users.noreply.github.com>
2023-11-22 09:18:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('First Appearence')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free Range Egg')
+
