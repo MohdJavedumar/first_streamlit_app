bc32ce18057690abb564073e58abaa6361a178fb
Mohd Javed <63085243+MohdJavedumar@users.noreply.github.com>
2023-12-02 15:01:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,4 +30,4 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-select * from util_db.public.fruit_load_list
+# select * from util_db.public.fruit_load_list
