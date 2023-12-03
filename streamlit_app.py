7d60f9fa31c168e2d3d85d2d8fabb776bad1fbd0
Mohd Javed <63085243+MohdJavedumar@users.noreply.github.com>
2023-12-03 07:47:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,4 @@ streamlit.text(my_data_row)
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
 my_cur.execute ("insert into fruit_load_list values ('from streamlit')")
+from urllib.error import URLError
